84e4d6f14bb342a8a44e7ba84eff8b146a3bdcea
Ігор Кіцул <>
2022-11-12 14:46:18 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int main(void)  {
+int a;
+printf("Увідіть значення ребра ");
+scanf("%d",&a);
+    
+printf ("Площя Куба S =%d\n",6*a*a);
+    
+printf ("Площя Куба V =%d\n",a*a*a);   
+  return 0;
+}
